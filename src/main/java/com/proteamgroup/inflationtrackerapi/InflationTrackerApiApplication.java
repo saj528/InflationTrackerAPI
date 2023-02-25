f1b7f8cfd0936223564fcbcd205079bef8fbe6d4
ajpc <alexjoinerweb@gmail.com>
2023-02-24 18:15:54 -0600

    Created the Spring App

---
+++ src/main/java/com/proteamgroup/inflationtrackerapi/InflationTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.proteamgroup.inflationtrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflationTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflationTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proteamgroup/inflationtrackerapi/InflationTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proteamgroup.inflationtrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflationTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
